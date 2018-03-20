7bed6d5d2a0ace0c1b126f8f18120527204b7596
ahmad solehin <solehinjr@gmail.com>
2018-03-20 18:11:48 +0800

    first commit

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,191 @@
+@extends('welcome')
+
+@section('content')
+
+
+
+<div class="container">
+
+
+<!-- View Data -->
+<table class="table">
+		<thead class="thead-dark">
+		<th colspan="5">Members</th>
+		<tr>
+			<td>Id</td>
+			<td>Name</td>
+			<td>Age</td>
+			<td>Email</td>
+			<td>Address</td>
+		</tr>
+
+		@foreach($member as $value)
+		<tr>
+			<td>{{$value->id}}</td>
+			<td>{{$value->name}}</td>
+			<td>{{$value->age}}</td>
+			<td>{{$value->email}}</td>
+			<td>{{$value->address}}</td>
+		</tr>
+		@endforeach
+	</table>
+
+	<!-- Insert Data -->
+
+ <table class="table">
+    <thead class="thead-dark">
+
+    		<th colspan="2"> Insert </th>
+		<tr>
+			<td>Name :</td>
+			<td><input type="text" class="form-control" name="name" /></td>
+		</tr>
+		<tr>
+			<td>Age :</td>
+			<td><input type="text" class="form-control" name="age" /></td>
+		</tr>
+		<tr>
+			<td>Email</td>
+			<td><input type="text" class="form-control" name="email" /></td>
+		</tr>
+		<tr>
+			<td>Address</td>
+			<td><input type="text" class="form-control" name="address" /></td>
+		</tr>
+		<tr>
+			<td colspan="2">
+<button type="button" id="insert" class="btn btn-success">Insert</button>
+
+			 </td>
+		</tr>
+	</table>
+	{{ csrf_field() }} <!-- this is important for generate token -->
+
+	<!-- Update Data -->
+
+ <table class="table">
+    <thead class="thead-dark">
+
+    		<th colspan="2">Update</th>
+		<tr>
+			<td>Select Id:</td>
+			<td>
+				<select class="form-control" name="upid" id="upid">
+					@foreach($member as $value)
+					<option value="{{ $value->id}}"> {{ $value->id }} </option>
+					@endforeach
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td>Name :</td>
+			<td><input type="text" class="form-control" name="upname" /></td>
+		</tr>
+		<tr>
+			<td>Age :</td>
+			<td><input type="text" class="form-control" name="upage" /></td>
+		</tr>
+		<tr>
+			<td>Email</td>
+			<td><input type="text" class="form-control" name="upemail" /></td>
+		</tr>
+		<tr>
+			<td>Address</td>
+			<td><input type="text" class="form-control" name="upaddress" /></td>
+		</tr>
+		<tr>
+			<td colspan="2">
+						<button type="button" id="update" class="btn btn-warning">Update</button>
+
+			 </td>
+		</tr>
+	</table>
+
+	<!-- Delete Data -->
+
+	<table class="table">
+		<thead class="thead-dark">
+
+				<th colspan="2"> Delete </th>
+		<tr>
+			<td>Select Id:</td>
+			<td>
+				<select class="form-control" name="upid" id="delid">
+					@foreach($member as $value)
+					<option value="{{ $value->id}}"> {{ $value->id }} </option>
+					@endforeach
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td colspan="2">
+			<button type="button" id="delete" class="btn btn-danger">Delete</button>
+
+			 </td>
+		</tr>
+	</table>
+
+
+
+
+</div>
+
+
+	<!-- AJAX SECTION -->
+
+	<script type="text/javascript">
+				// for Insert Ajax
+				$('#insert').click(function(){
+					$.ajax({
+						type:'post',
+						url: 'insertdata',
+						data:{
+							'_token':$('input[name=_token').val(),
+							'name':$('input[name=name').val(),
+							'age':$('input[name=age').val(),
+							'email':$('input[name=email').val(),
+							'address':$('input[name=address').val()
+						},
+						success:function(data){
+							window.location.reload();
+						},
+					});
+				});
+
+				// for Update Ajax
+				$('#update').click(function(){
+					$.ajax({
+						type:'post',
+						url: 'updatedata',
+						data:{
+							'_token':$('input[name=_token').val(),
+							'id':$('#upid').val(),
+							'name':$('input[name=upname').val(),
+							'age':$('input[name=upage').val(),
+							'email':$('input[name=upemail').val(),
+							'address':$('input[name=upaddress').val()
+						},
+						success:function(data){
+							window.location.reload();
+						},
+					});
+				});
+
+				// for Delete Ajax
+				$('#delete').click(function(){
+					$.ajax({
+						type:'post',
+						url: 'deletedata',
+						data:{
+							'_token':$('input[name=_token').val(),
+							'id':$('#delid').val(),
+						},
+						success:function(data){
+							window.location.reload();
+						},
+					});
+				});
+			</script>
+
+			
+			@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                @yield('content')
+            </div>
+        </div>
+    </body>
+</html>
